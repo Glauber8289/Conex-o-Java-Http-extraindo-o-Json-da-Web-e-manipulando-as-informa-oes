bf841de4e4a204f62202fa334a879854469f161a
Glauber Peterson <Petersonglauber@hotmail.com>
2022-07-23 23:54:11 -0300

    Acrescentando notas e estrelinhas

--- src/GeradoradeFigurinhas.java
+++ src/GeradoradeFigurinhas.java
@@ -1,18 +1,21 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
-import java.net.URL;
-import java.awt.Color;
-import java.awt.Font;
+
 import javax.imageio.ImageIO;
 import javax.swing.plaf.FontUIResource;
 
 public class GeradoradeFigurinhas {
 
-    void cria() throws Exception {
-
-        InputStream inputStream = new URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_3.jpg").openStream();
+    void cria(InputStream inputStream, String nomeArquivo) throws Exception {
+        // leitura de imagem
+        // InputStream inputStream =
+        // new FileInputStream(new File("entrada/filme.jpg"));
+        // InputStream inputStream = new
+        // URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_3.jpg").openStream();
         BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
         // cria nova imagem em memória com transparencia e com tamanho novo
@@ -23,7 +26,7 @@ public class GeradoradeFigurinhas {
 
         // copiar a imagem original para nova imagem (em memória)
         Graphics2D graphics = (Graphics2D) novaImagem.createGraphics();
-        graphics.drawImage(imagemOriginal, 0, 0, null);
+        graphics.drawImage(imagemOriginal, 50, 50, null);
 
         // configurar a fonte
         var fonte = new FontUIResource(Font.SANS_SERIF, Font.BOLD, 70);
@@ -31,16 +34,11 @@ public class GeradoradeFigurinhas {
         graphics.setFont(fonte);
 
         // escrever uma frase na nova imagem
-        graphics.drawString("TOPZERA", 0, novaAltura - 100);
+        graphics.drawString("TOPZERA", 500, novaAltura - 100);
 
         // escrever a nova imagem em um arquivo
-        ImageIO.write(novaImagem, "png", new File("Figurinha.png"));
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
 
-    }
-
-    public static void main(String[] args) throws Exception {
-        var geradora = new GeradoradeFigurinhas();
-        geradora.cria();
     }
 
 }
